de73e5fe05076278e87153cc573f3f83e7d26b8c
chebihiF <chebihi.f@ucd.ac.ma>
2022-04-26 23:38:24 +0000

    Initial commit

---
+++ product-service/src/test/java/suptech/miag/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package suptech.miag.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ proxy-server/src/test/java/suptech/miag/proxyserver/ProxyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package suptech.miag.proxyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
