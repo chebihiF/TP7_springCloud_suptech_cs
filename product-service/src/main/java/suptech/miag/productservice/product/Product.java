a31891395204977b636f34843424c79d21e51243
chebihiF <chebihi.f@ucd.ac.ma>
2022-05-17 20:26:52 +0100

    add product service

---
+++ product-service/src/main/java/suptech/miag/productservice/product/Product.java
@@ -0,0 +1,19 @@
+package suptech.miag.productservice.product;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity @Getter @Setter @AllArgsConstructor @NoArgsConstructor
+public class Product {
+    @Id @Column(length = 30)
+    private String ref ;
+    private String label ;
+    private int quantity ;
+    private double price ;
+}
